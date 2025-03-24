80a1de6934bab5c159bb5e309b6cbfec8a5c2b5b
Ajasra <ajasra@INLP062201.local>
2025-03-25 03:29:08 +0530

    feat: updates size of archive thumb images to be 3;2 and added plugin to regenerate the thumbs

--- wp-content/themes/gafas-theme/inc/image.php
+++ wp-content/themes/gafas-theme/inc/image.php
@@ -1,6 +1,7 @@
 <?php
 if ( function_exists ( 'add_image_size' ) ) :
-	add_image_size( 'prod-thumb', 540, 540, true );
+	// add_image_size( 'prod-thumb', 540, 540, true );
+	add_image_size( 'prod-thumb', 400, 267, true );
 	add_image_size( 'shop-banner', 800, 200, true);
 	add_image_size( 'shop-taxo', 460, 600, true);
 	add_image_size( 'prod-single', 600, 900, false);
